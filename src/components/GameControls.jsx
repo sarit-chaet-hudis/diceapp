03219aea87312179398fe93660ff3dd270d68539
sarit-chaet-hudis <sarit.chh@gmail.com>
2021-12-16 21:37:36 +0200

    basic game logic down

--- src/components/GameControls.jsx
+++ src/components/GameControls.jsx
@@ -2,14 +2,25 @@ import React from "react";
 import Die from "./Die";
 
 class GameControls extends React.Component {
+  roll = () => {
+    const result = Math.ceil(Math.random() * 11) + 1;
+    console.log(`result of throw dice is ${result}`);
+    this.props.addDiceResult(result);
+  };
+
   render() {
     return (
       <>
         hi Im Game Controls
+        <p>Current score is: {this.props.currentScore}</p>
         <Die />
         <Die />
-        <button id="roll">Roll</button>
-        <button id="hold">Hold</button>
+        <button id="roll" onClick={this.roll}>
+          Roll
+        </button>
+        <button id="hold" onClick={this.props.hold}>
+          Hold
+        </button>
       </>
     );
   }
--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -6,13 +6,39 @@ import Player from "../components/Player";
 class Game extends React.Component {
   state = {
     currentScore: 0,
-    player1: { id: 1, score: 0, isActive: false },
-    player2: { id: 2, score: 0, isActive: true },
+    player0: { id: 0, score: 0, isActive: false },
+    player1: { id: 1, score: 0, isActive: true },
   };
 
   switchTurns = () => {
+    //TODO find out if this syntax is doing what i thought and why no : is involved
+    this.setState((prev) => (prev.player0.isActive = !prev.player0.isActive));
     this.setState((prev) => (prev.player1.isActive = !prev.player1.isActive));
-    this.setState((prev) => (prev.player2.isActive = !prev.player2.isActive));
+  };
+
+  addDiceResult = (result) => {
+    this.setState((prev) => {
+      return { currentScore: prev.currentScore + result };
+    });
+  };
+
+  getActivePlayer() {
+    const playerArray = [this.state.player0, this.state.player1];
+    const activePlayer = playerArray.find((p) => p.isActive);
+    return activePlayer;
+  }
+
+  hold = () => {
+    // add current score to total player score, then reset current score
+    const active = this.getActivePlayer();
+    console.log(`active player is ${active.id}`);
+    const activePlayerCopy = Object.assign({}, active);
+    activePlayerCopy.score += this.state.currentScore;
+    console.log(`new score of active copy is ${activePlayerCopy.score}`);
+
+    this.setState({ [`player${active.id}`]: activePlayerCopy });
+    this.setState({ currentScore: 0 });
+    this.switchTurns();
   };
 
   render() {
@@ -22,18 +48,22 @@ class Game extends React.Component {
 
         <div className="playerContainer">
           <Player
+            id={this.state.player0.id}
+            score={this.state.player0.score}
+            isActive={this.state.player0.isActive}
+          />
+          <Player
             id={this.state.player1.id}
             score={this.state.player1.score}
             isActive={this.state.player1.isActive}
           />
-          <Player
-            id={this.state.player2.id}
-            score={this.state.player2.score}
-            isActive={this.state.player2.isActive}
-          />
         </div>
         <div className="gameControls">
-          <GameControls />
+          <GameControls
+            currentScore={this.state.currentScore}
+            addDiceResult={this.addDiceResult}
+            hold={this.hold}
+          />
         </div>
       </div>
     );
