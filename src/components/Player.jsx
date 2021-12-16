54cf57e373e68027e6f9539eca7568a766f71475
sarit-chaet-hudis <sarit.chh@gmail.com>
2021-12-16 17:44:35 +0200

    useless constructor

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,6 @@
 import React from "react";
 
 class Player extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     // const { id, myScore } = this.props;
     return (
